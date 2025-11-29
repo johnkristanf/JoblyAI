74d86959447d0106cdc6d80b35d0fd8164a5b4ad
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-11-29 17:07:27 +0800

    chore: cors and change import path

--- server/src/main.py
+++ server/src/main.py
@@ -4,33 +4,30 @@ from fastapi.middleware.cors import CORSMiddleware
 from openai import OpenAI
 
 from src.config import settings
-from src.jobs.router import jobs_router
+from src.job.router import job_router
 from src.utils import group
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    app.state.openai_client = OpenAI(api_key=settings.OPENAI_API_KEY)
-    yield
-    app.state.openai_client = None
 
 
 api_v1_router = group(
     "/api/v1",
-    (jobs_router, "/jobs", ["Jobs"]),
+    (job_router, "/job", ["Job"]),
 )
+
+app.include_router(api_v1_router)
 
 
 @app.get("/health")
-def health():
+def check_server_health():
     return {"message": "Server is Healthy"}
