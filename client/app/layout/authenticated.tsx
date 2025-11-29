e0fd6e92a86243c34021a6ac85ae6bfd2118a36a
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-11-29 17:05:55 +0800

    query client provider

--- client/app/layout/authenticated.tsx
+++ client/app/layout/authenticated.tsx
@@ -1,15 +1,21 @@
-import { Outlet } from "react-router";
-import { AppSidebar } from "~/components/app-sidebar";
-import { SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
+import { Outlet } from 'react-router'
+import { AppSidebar } from '~/components/app-sidebar'
+import { SidebarProvider, SidebarTrigger } from '~/components/ui/sidebar'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function AuthenticatedLayout() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarTrigger className="mt-2 pl-3" />
-      <main>
-        <Outlet />
-      </main>
-    </SidebarProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <SidebarProvider>
+                <AppSidebar />
+                <main className="w-full ">
+                    <SidebarTrigger className="mt-2 pl-3 hover:cursor-pointer hover:opacity-75" />
+                    <Outlet />
+                </main>
+            </SidebarProvider>
+        </QueryClientProvider>
+    )
 }
