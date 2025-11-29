2d1b5277d277feab5fd23294855dd93c8880ee32
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-11-30 07:04:25 +0800

    save job in schema

--- server/src/job/schema.py
+++ server/src/job/schema.py
@@ -8,3 +8,21 @@ class JobsSearchIn(BaseModel):
     country: str
     professional_summary: str
 
+
+class SaveJobIn(BaseModel):
+    job_title: str | None = None
+    job_description: str | None = None
+    job_employment_type: str | None = None
+    job_apply_link: str | None = None
+    job_apply_is_direct: bool | None = None
+    job_is_remote: bool | None = None
+
+    employer_name: str | None = None
+    employer_logo: str | None = None
+    employer_website: str | None = None
+
+    job_min_salary: float | None = None
+    job_max_salary: float | None = None
+    job_salary_period: str | None = None
+
+    extraction_note: str | None = None
