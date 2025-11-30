ddc57a4e524eb2b2f54036c61c16d4a6c775fd96
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-11-30 09:37:48 +0800

    update company logo

--- client/app/components/app-sidebar.tsx
+++ client/app/components/app-sidebar.tsx
@@ -1,59 +1,57 @@
-import { BriefcaseBusiness, FileUser } from "lucide-react";
-import { NavLink } from "react-router";
+import { BookmarkCheck, BriefcaseBusiness, FileUser } from 'lucide-react'
+import { NavLink } from 'react-router'
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "~/components/ui/sidebar";
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from '~/components/ui/sidebar'
 
 export function AppSidebar() {
-  return (
-    <Sidebar>
-      <SidebarHeader>
-        <div className="flex items-center gap-2">
-          <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-            <img src="/favicon.ico" className="size-4" />
-          </div>
-          <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate font-medium text-blue-600">JoblyAI</span>
-          </div>
-        </div>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Projects</SidebarGroupLabel>
+    return (
+        <Sidebar>
+            <SidebarHeader>
+                <div className="flex items-center ">
+                    <div className=" flex aspect-square size-12 items-center justify-center rounded-lg">
+                        <img src="/joblyai-logo-bg_removed.png" className="size-12" />
+                    </div>
+                    <div className="grid flex-1 text-left text-sm leading-tight">
+                        <span className="truncate font-medium text-blue-600">JoblyAI</span>
+                    </div>
+                </div>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Projects</SidebarGroupLabel>
 
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton>
-                <NavLink to="/job/search" className="flex items-center gap-1 ">
-                  <BriefcaseBusiness className="size-4" />{" "}
-                  <span className="text-xs">Job Search</span>
-                </NavLink>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <SidebarMenuButton>
+                                <NavLink to="/job/search" className="flex items-center gap-1 ">
+                                    <BriefcaseBusiness className="size-4" />{' '}
+                                    <span className="text-xs">Job Search</span>
+                                </NavLink>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
 
-            <SidebarMenuItem>
-              <SidebarMenuButton>
-                <NavLink
-                  to="/resume/builder"
-                  className="flex items-center gap-1"
-                >
-                  <FileUser className="size-4" />{" "}
-                  <span className="text-xs">Resume Builder</span>
-                </NavLink>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter />
-    </Sidebar>
-  );
+                        <SidebarMenuItem>
+                            <SidebarMenuButton>
+                                <NavLink to="/saved/jobs" className="flex items-center gap-1">
+                                    <BookmarkCheck className="size-4" />{' '}
+                                    <span className="text-xs">Saved Jobs</span>
+                                </NavLink>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+
+                    </SidebarMenu>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter />
+        </Sidebar>
+    )
 }
