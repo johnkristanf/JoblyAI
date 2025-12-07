6e21c48bd3247ac752155fcb489df3e45fe40f83
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-12-08 07:45:18 +0800

    api request for upload avatar

---
+++ client/app/lib/api/patch.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import { getAccessToken } from '../supabase/client'
+
+export const uploadAvatar = async (formData: FormData) => {
+    const accessToken = await getAccessToken()
+
+    const response = await axios.patch(
+        `${import.meta.env.VITE_API_V1_BASE_URL}/user/profile`,
+        formData,
+        {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+            },
+        },
+    )
+
+    return response.data
+}
