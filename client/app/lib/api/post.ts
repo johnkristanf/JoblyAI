7073e91b591e93631104ce8292cdae89e3066c75
John Kristan Torremocha <torremocha.johnkristan@gmailo.com>
2025-12-12 05:13:15 +0800

    formData param

--- client/app/lib/api/post.ts
+++ client/app/lib/api/post.ts
@@ -3,31 +3,20 @@ import type { Dispatch, SetStateAction } from 'react'
 import type { JobMatch, JobSearchForm, JobSearchResponse } from '~/types/job_search'
 import { getAccessToken, supabase } from '../supabase/client'
 
-export const jobSearch = async ({
-    payload,
-    setJobSearchResponse,
-}: {
-    payload: JobSearchForm
-    setJobSearchResponse: Dispatch<SetStateAction<JobSearchResponse | undefined>>
-}) => {
+export const jobSearch = async (formData: FormData) => {
     const accessToken = await getAccessToken()
 
     const response = await axios.post(
         `${import.meta.env.VITE_API_V1_BASE_URL}/job/search`,
-        payload,
+        formData,
         {
             headers: {
+                'Content-Type': 'multipart/form-data',
                 Authorization: accessToken ? `Bearer ${accessToken}` : '',
-                'Content-Type': 'application/json',
             },
         },
     )
-    const undefinedValueCatcher = {
-        job_listings: [],
-        jobs_matched: [],
-    }
 
-    setJobSearchResponse(response.data ?? undefinedValueCatcher)
     return response.data
 }
 
