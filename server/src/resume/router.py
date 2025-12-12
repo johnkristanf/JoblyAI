5bc54f704e3ebf9636d4756d8840f5beb0d25c12
John Kristan Torremocha <138425887+johnkristanf@users.noreply.github.com>
2025-12-13 04:02:56 +0800

    feat: support multi file for resume upload

--- server/src/resume/router.py
+++ server/src/resume/router.py
@@ -11,18 +11,38 @@ resume_router = APIRouter()
 s3 = boto3.client("s3", region_name=settings.AWS_REGION)
 
 @resume_router.post("/upload")
-async def upload_resume(resume: UploadFile = File(...)):
-    object_key = f"resumes/{uuid.uuid4()}_{resume.filename}"
-    file_content = await resume.read()
-    try:
-        s3.put_object(
-            Bucket=settings.AWS_S3_BUCKET_NAME,
-            Key=object_key,
-            Body=file_content,
-            ContentType=resume.content_type,
-        )
-        resume_url = f"https://{settings.AWS_S3_BUCKET_NAME}.s3.{settings.AWS_REGION}.amazonaws.com/{object_key}"
-        return {"message": "Resume uploaded successfully", "url": resume_url}
-    except Exception as e:
-        return {"error": str(e)}
+async def upload_resume(resume: list[UploadFile] = File(...)):
+    print(f"resume: {resume}")
+    uploaded_files = []
+    for file in resume:
+        object_key = f"resumes/{uuid.uuid4()}_{file.filename}"
+        file_content = await file.read()
+        try:
+            s3.put_object(
+                Bucket=settings.AWS_S3_BUCKET_NAME,
+                Key=object_key,
+                Body=file_content,
+                ContentType=file.content_type,
+            )
+            resume_url = (
+                f"https://{settings.AWS_S3_BUCKET_NAME}.s3.{settings.AWS_REGION}.amazonaws.com/{object_key}"
+            )
+            uploaded_files.append(
+                {
+                    "filename": file.filename,
+                    "url": resume_url,
+                    "content_type": file.content_type,
+                }
+            )
+        except Exception as e:
+            uploaded_files.append(
+                {
+                    "filename": file.filename,
+                    "error": str(e),
+                }
+            )
+    return {
+        "message": "Resume(s) uploaded",
+        "files": uploaded_files,
+    }
 
