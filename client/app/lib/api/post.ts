357fe6e1d0be86e9932f70bac607b32d5e67505c
John Kristan Torremocha <138425887+johnkristanf@users.noreply.github.com>
2025-12-13 04:02:00 +0800

    resume upload api

--- client/app/lib/api/post.ts
+++ client/app/lib/api/post.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
-import type { Dispatch, SetStateAction } from 'react'
-import type { JobMatch, JobSearchForm, JobSearchResponse } from '~/types/job_search'
+import type { JobMatch,  } from '~/types/job_search'
 import { getAccessToken, supabase } from '../supabase/client'
+import type { ResumeFile } from '~/types/resume'
 
 export const jobSearch = async (formData: FormData) => {
     const accessToken = await getAccessToken()
@@ -39,3 +39,27 @@ export const saveJob = async (jobToSave: JobMatch) => {
         throw new Error(error.response?.data?.message || 'Failed to save job')
     }
 }
+
+
+export const uploadResume = async (files: ResumeFile[]) => {
+    const accessToken = await getAccessToken();
+    const formData = new FormData();
+
+    files.forEach((file, idx) => {
+        formData.append('resume', file.file);
+    });
+
+    const response = await axios.post(
+        `${import.meta.env.VITE_API_V1_BASE_URL}/resume/upload`,
+        formData,
+        {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                Authorization: accessToken ? `Bearer ${accessToken}` : '',
+            },
+        }
+    );
+
+    return response.data;
+};
+
---
+++ client/app/types/resume.ts
@@ -0,0 +1,6 @@
+export type ResumeFile = {
+    id: number;
+    fileName: string;
+    file: File;
+    previewImgUrl?: string;
+  };
